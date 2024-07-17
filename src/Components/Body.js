0f2ac4dd31fa921f6cdc27e26bd43145f4719766
AmtulKhan <amtulmaswaraahmed@gmail.com>
2024-07-17 18:44:22 +0530

    components started

---
+++ src/Components/Body.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./login";
+import Browse from "./Browse";
+
+const Body = () => {
+  return (
+    <div>
+      <Login />
+      <Browse />
+    </div>
+  );
+};
+
+export default Body;
