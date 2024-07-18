36162eb3b74c4e3a3f27577e87b76d5f74d1628b
AmtulKhan <amtulmaswaraahmed@gmail.com>
2024-07-18 22:55:52 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Body from "./Components/Body";
 
 function App() {
--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,12 +1,22 @@
 import React from "react";
-import Login from "./login";
+import Login from "./Login.js";
 import Browse from "./Browse";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
   return (
     <div>
-      <Login />
-      <Browse />
+      <RouterProvider router={appRouter} />
     </div>
   );
 };
