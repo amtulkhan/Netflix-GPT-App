5f84e1f9ca68c15c7499f346287219513585ef5d
AmtulKhan <amtulmaswaraahmed@gmail.com>
2024-11-13 23:15:33 +0530

    sign up form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -16,8 +16,17 @@ const Login = () => {
           alt="background"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+      <form className="w-3/12 absolute p-10 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignIn && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
@@ -29,9 +38,13 @@ const Login = () => {
           className="p-4 my-4 w-full bg-gray-700"
         />
         <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-          Sign In
+          {isSignIn ? "Sign In" : "Sign Up"}
         </button>
-        <p onClick={toggleSignInForm}>New to Netflix? Sign up now</p>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignIn
+            ? "New to Netflix? Sign up now"
+            : "Already registered.Sign in now"}
+        </p>
       </form>
     </div>
   );
